e1e53977318a60f495491d6e97de602e3a0ddee6
delevinying <delevinyyc@gmail.com>
2019-02-19 09:36:12 +0800

    .

---
+++ 404.sum-of-left-leaves.java
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=404 lang=java
+ *
+ * [404] 左叶子之和
+ *
+ * https://leetcode-cn.com/problems/sum-of-left-leaves/description/
+ *
+ * algorithms
+ * Easy (48.15%)
+ * Total Accepted:    4.2K
+ * Total Submissions: 8.8K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 计算给定二叉树的所有左叶子之和。
+ * 
+ * 示例：
+ * 
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * 在这个二叉树中，有两个左叶子，分别是 9 和 15，所以返回 24
+ * 
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    private int sum =0;
+    public int sumOfLeftLeaves(TreeNode root) {
+        if(root==null){return 0;}
+        if(root.left!=null && root.left.left == null && root.left.right == null){
+            sum += root.left.val;
+        }
+        sumOfLeftLeaves(root.left);
+        sumOfLeftLeaves(root.right);
+        return sum;
+        // int left = (root.left==null)?0:root.left.val;
+        // sum += left + sumOfLeftLeaves(root.left)
+    }
+}
